2415a845877d2ff8daca8406cbff57e1bd54f2f4
jeric realubit <jericrealubit@gmail.com>
2022-09-02 09:39:46 +1200

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# api-thu
+
+## REST API with express / mongodb / netlify / serverless 
+
+# GET
+https://rest-api-express-mongodb.netlify.app/.netlify/functions/api/
+
+# POST
+https://rest-api-express-mongodb.netlify.app/.netlify/functions/api/
+PAYLOAD:
+{    
+    "username": "johndoe",
+    "email": "johndoe@gmail.com",
+    "imageUrl": "abc"
+}
+
+# GET:id
+https://rest-api-express-mongodb.netlify.app/.netlify/functions/api/62f2e9c9d9bcc19cd5dbcc88
+
+# PUT:id
+https://rest-api-express-mongodb.netlify.app/.netlify/functions/api/62f2e9c9d9bcc19cd5dbcc88
+PAYLOAD:
+{    
+    "username": "NEWjohndoe",
+    "email": "johndoe@gmail.com",
+    "imageUrl": "abc"
+}
+
+# DELETE:id
+https://rest-api-express-mongodb.netlify.app/.netlify/functions/api/62f2e9c9d9bcc19cd5dbcc88
---
+++ src/api.js
@@ -0,0 +1,65 @@
+const express = require("express")
+const cors = require('cors')
+const serverless = require("serverless-http")
+const { collection, ObjectId } = require("../config/database")
+
+const app = express()
+const router = express.Router()
+
+app.use(cors())
+
+// needed for adding a document 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+router.get("/", (req, res) => {
+    //res.send("User List")
+    collection.find().toArray((err, result) => {
+        if (err) throw err;
+        res.json(result)
+    })
+})
+
+router.post("/", (req, res) => {
+    // res.send("Create New User")
+     collection.insertOne(req.body, (err, res) => {
+         if (err) throw err;
+     })
+    res.send("1 document inserted.")
+ })
+ 
+ router
+     .route("/:id")
+     .get((req, res) => {
+        //res.send(`Get a User with ID: ${req.params.id}`)
+        const query = {
+             _id: ObjectId(req.params.id)
+        }
+        collection.findOne(query, (err, result) => {
+             if (err) throw err;
+             res.send(result)
+        })
+     })
+     .put((req, res) => {
+         //res.send(`Update a User with ID: ${req.params.id}`)
+         const query = { _id: ObjectId(req.params.id) }
+         let newvalues = { $set: req.body };
+         collection.updateOne(query, newvalues, (err, result) => {
+             if (err) throw err;
+         })
+         res.send("1 document updated.")
+     })
+     .delete((req, res) => {
+         //res.send(`Delete a User with ID: ${req.params.id}`)
+         const query = { _id: ObjectId(req.params.id) }
+         collection.deleteOne(query, (err, result) => {
+             if (err) throw err;
+         })
+         res.send("1 document deleted.")
+     })
+
+app.use("/.netlify/functions/api", router)
+module.exports.handler = serverless(app)
